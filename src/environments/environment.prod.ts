db5390be80043dae43b4ebe6826b8120f9ddc857
SamuelOsp <avilaospina1@gmail.com>
2025-11-28 18:36:51 -0500

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,13 @@ export const environment = {
   supabase: {
     url: 'https://liwszjdrqqnxcforrmar.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxpd3N6amRycXFueGNmb3JybWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MzQ1OTksImV4cCI6MjA3OTUxMDU5OX0.q8Lj2wqvNVH6lRAqrnNTQG93I1fAAZltAB_EoWf-uQk'
+  },
+  firebase: {
+    apiKey: "AIzaSyBY48U0AQJRivsfxHx2kNkgvmB3IWcG22Y",
+    authDomain: "ar-app-aee80.firebaseapp.com",
+    projectId: "ar-app-aee80",
+    storageBucket: "ar-app-aee80.firebasestorage.app",
+    messagingSenderId: "974917817002",
+    appId: "1:974917817002:web:1ab35412e03f529b2bf6d3"
   }
 };
